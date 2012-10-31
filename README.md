e34421c33e2eabf10fd0115ede17160febccd4cc
dsboger <dsboger@gmail.com>
2012-10-31 10:05:22 -0200

    added proper licensing

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Alt Tab Mod: Always Show Thumbnails
 
 GNOME Shell extension. Supports GNOME Shell 3.6.
 Extension page: https://extensions.gnome.org/extension/516/force-thumbnails-on-alt-tab/
+Github: https://github.com/dsboger/gs-ext-alttabmod-thumbnails
 
 Default Alt+Tab behavior is to wait around half a second before showing window thumbnails or not to show thumbnails at all for single window applications. This extension forces Alt+Tab popup to always show window thumbnails of the selected app. This way you can always preview the window you are jumping to.
 
--- extension.js
+++ extension.js
@@ -1,4 +1,20 @@
-
+/*
+ * Alt Tab Mod: Always Show Thumbnails - GNOME Shell extension
+ * Copyright (C) 2012  Davi da Silva Böger  (dsboger@gmail.com)
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
 const AltTab = imports.ui.altTab;
